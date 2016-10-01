4f1b6fb6883d848f6f5b9e39c05803142b7d7532
bradshawit <nathan@bradshawit.com.au>
2016-10-02 06:54:21 +1100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -6,7 +6,8 @@ cd /opt/sickrage
 git pull
 
 #service sickrage start &
-exec /opt/sickrage/SickBeard.py --pidfile=/data/sickrage/.sickrage.pid --datadir=/data/sickrage &
+#exec /opt/sickrage/SickBeard.py --pidfile=/data/sickrage/.sickrage.pid --datadir=/data/sickrage &
+python /opt/sickrage/SickBeard.py --pidfile=/data/sickrage/.sickrage.pid --datadir=/data/sickrage &
 wait
 
 
